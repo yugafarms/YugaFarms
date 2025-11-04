2dcb6167e5decbc9dbdc651bef76c80c7b1501b2
ayushkumarsingh2422005 <ayush2422005@gmail.com>
2025-11-05 02:15:53 +0530

    image path update

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,11 @@ const nextConfig: NextConfig = {
         port: "1337",
         pathname: "/uploads/**",
       },
+      {
+        protocol: "https",
+        hostname: "server.yugafarms.com",
+        pathname: "/uploads/**",
+      },
     ],
   },
 };
